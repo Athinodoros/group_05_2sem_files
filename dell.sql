bf938de2e7f664a8860f3ae39bf3638f7846f2ae
Bancho Petrov <bancho_p@abv.bg>
2015-04-08 13:41:36 +0200

    Added backlog, rel schema and er

---
+++ dell.sql
@@ -0,0 +1,52 @@
+
+alter session set nls_date_format = 'yyyy-mm-dd';
+
+
+drop table media;
+drop table projects;
+drop table users;
+drop table companies;
+
+create table companies (
+    companyname varchar(30),
+    budget integer,
+    primary key(companyname));
+
+create table users(
+  userid      integer,
+  uname     varchar(30),
+  password     varchar(32),
+  email     varchar(30),
+  country     varchar(30),
+  urole varchar(30),
+  company varchar(30),
+  foreign key (company) references companies(companyname),
+  primary key (userid));
+
+create table projects(
+  projectid integer,
+  authorid integer,
+  title varchar(30),
+  startdate date,
+  enddate date,
+  stage varchar(30),
+  budget integer,
+  poe varchar(30),
+  comments varchar(30),
+  constraint paFK foreign key(authorid) references users(userid),
+  primary key(projectid));
+
+create table media (
+  projectid integer,
+  userid integer,
+  filepath varchar(30),
+  constraint muFK foreign key (userid) references users(userid),
+  foreign key (projectid) references projects(projectid),
+  primary key(projectid));
+
+
+Insert into companies values('Dellyrom',322321);
+Insert into users values(4,'pepinio','password','email@ema','Danmark','Manager','Dellyrom');
+Insert into projects values(4,4,'Del','2011-12-12','2014-12-12','Pornich',1324,'Yes','Full of shits');
+Insert into media values(4,4,'Filepath//LocalDiskCassdannn');
+commit;
